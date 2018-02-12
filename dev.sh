be162ea08c1568d42c6d0aa2b24471d7e8c477eb
Carl Foster <carl.john.foster@gmail.com>
2018-02-12 15:39:37 +1000

    Improve the dev experience to not use root account

--- dev.sh
+++ dev.sh
@@ -3,7 +3,8 @@
 git submodule init
 git submodule update
 
-echo "Run ./autogen.sh && ./configure to finalise the setup"
-echo "Compile using make. Ensure to do a full rm of the stellar stack to deploy changes"
-docker run -v $(pwd):/usr/src -w /usr/src -it abxit/stellar-core-dev sh -c "useradd $USER && sudo -u $USER sh -c \"cd /usr/src && ./autogen.sh && ./configure && bash\""
-
+echo 'Compile using make. Ensure to do a full rm of the stellar stack to deploy changes'
+docker run -v $(pwd):/usr/src -w /usr/src -it abxit/stellar-core-dev sh -c "useradd $USER \
+    && su $USER -c sh -c \"cd /usr/src && source ./setup.sh && ./autogen.sh && ./configure \
+    && echo 'Compile using make. Ensure to do a full rm of the stellar stack to deploy changes' \
+    && bash\" "
