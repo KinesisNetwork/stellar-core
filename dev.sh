e709a55024f4370b96c64dafcfe1c16de60c034c
Sam <sam.jeston@gmail.com>
2018-02-14 17:50:09 +1000

    ABX-4086 Allow tests passing against a PG db

--- dev.sh
+++ dev.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-
 git submodule init
 git submodule update
 
-echo 'Compile using make. Ensure to do a full rm of the stellar stack to deploy changes'
-docker run -v $(pwd):/usr/src -w /usr/src -it abxit/stellar-core-dev sh -c "useradd $USER \
-    && su $USER -c sh -c \"cd /usr/src && source ./setup.sh && ./autogen.sh && ./configure \
-    && echo 'Compile using make. Ensure to do a full rm of the stellar stack to deploy changes' \
-    && bash\" "
+echo "Run ./autogen.sh && ./configure to finalise the setup"
+echo "Compile using make. Ensure to do a full rm of the stellar stack to deploy changes"
+echo "\n"
+echo "On the pg container, run the loop declared in dev_db.sh as the postgres user"
+echo "\n"
+echo "To run the tests, set up the pg databases and then run:"
+echo "make -j3"
+echo "export ALL_VERSIONS=1"
+echo "make check"
+
+docker pull postgres:9.6
+docker pull abxit/stellar-core-dev
+docker run --name pgstellar --rm -d postgres:9.6
+docker run -v $(pwd):/usr/src -e PGHOST=pgstellar -e PGUSER=postgres --link pgstellar -w /usr/src --rm -it abxit/stellar-core-dev
